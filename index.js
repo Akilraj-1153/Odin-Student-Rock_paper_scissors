a4e79b62e094a83464ab1d93a7823cb0843e0139
AKILRAJ  N <122591298+Akilraj-1153@users.noreply.github.com>
2023-11-30 22:24:21 +0530

    completed the logic for js

--- index.js
+++ index.js
@@ -1,29 +1,45 @@
-function getComputerChoice(){
-    var text=[
-        "Rock",
-        "Paper",
-        "Sissor"
-    ];
-    var textIndex = Math.floor(Math.random()*text.length);
+function getComputerChoice() {
+    var text = ["Rock", "Paper", "Scissor"];
+    var textIndex = Math.floor(Math.random() * text.length);
     var randomText = text[textIndex];
     return randomText;
 }
 
 function playRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection){
+    if (playerSelection === computerSelection) {
         return "Tie";
-    }
-    else if(playerSelection ==="Rock" && computerSelection==="Sissor"){
-        return "You Won!, Rock beats Sissor";
-    }
-    else if(playerSelection ==="Rock" && computerSelection==="Paper"){
+    } else if (playerSelection === "Rock" && computerSelection === "Scissor") {
+        player += 1;
+        return "You Won!, Rock beats Scissor";
+    } else if (playerSelection === "Rock" && computerSelection === "Paper") {
+        computer += 1;
         return "You Lose! Paper beats Rock";
     }
-    console.log(playRound);
-  }
-   
-  const playerSelection = "Rock";
-  const computerSelection = getComputerChoice();
-  console.log("playerSelection"+" "+playerSelection);
-  console.log("computerSelection"+" "+computerSelection);
-  console.log(playRound(playerSelection, computerSelection));
+}
+
+function game() {
+    for (let i = 1; i <= 5; i++) {
+        const playerSelection = "Rock"; 
+        const computerSelection = getComputerChoice();
+        console.log("Round" + ":" + i);
+        console.log("playerSelection" + " : " + playerSelection);
+        console.log("computerSelection" + " : " + computerSelection);
+        console.log(playRound(playerSelection, computerSelection));
+        console.log("player point" + ":" + player);
+        console.log("computer point" + ":" + computer);
+    }
+
+    if (player >= 3 || (player > computer)) {
+        return "Player wins";
+    } else if (computer >= 3 || (player < computer)) {
+        return "Computer Wins";
+    } else {
+        return "Tie";
+    }
+    
+}
+
+let player = 0;
+let computer = 0;
+console.log(game());
+document.write(game());
