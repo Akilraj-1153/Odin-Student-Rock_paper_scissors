1d7ec893c57f580d798cbb1397c872b54d3ded70
AKILRAJ  N <122591298+Akilraj-1153@users.noreply.github.com>
2023-11-30 21:44:33 +0530

    wrote logic for 1 iteration

--- index.js
+++ index.js
@@ -1,5 +1,29 @@
-var a = 5;
-function js(){
-    document.getElementById("js").innerText = "Hello, world!";
+function getComputerChoice(){
+    var text=[
+        "Rock",
+        "Paper",
+        "Sissor"
+    ];
+    var textIndex = Math.floor(Math.random()*text.length);
+    var randomText = text[textIndex];
+    return randomText;
 }
 
+function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection){
+        return "Tie";
+    }
+    else if(playerSelection ==="Rock" && computerSelection==="Sissor"){
+        return "You Won!, Rock beats Sissor";
+    }
+    else if(playerSelection ==="Rock" && computerSelection==="Paper"){
+        return "You Lose! Paper beats Rock";
+    }
+    console.log(playRound);
+  }
+   
+  const playerSelection = "Rock";
+  const computerSelection = getComputerChoice();
+  console.log("playerSelection"+" "+playerSelection);
+  console.log("computerSelection"+" "+computerSelection);
+  console.log(playRound(playerSelection, computerSelection));
